a4e63b8758890cb49afceee7e49a435a3f9e8da2
Lina Khaled <linaxn4@gmail.com>
2025-10-16 15:21:17 +0300

    Add API tests for popular TV shows endpoint with RestAssured

---
+++ src/test/java/base_urls/MovieDBBaseUrl.java
@@ -0,0 +1,26 @@
+package base_urls;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MovieDBBaseUrl {
+
+    protected RequestSpecification spec;
+
+  Map<String,String>apiKey= new HashMap<>();
+
+    @BeforeMethod
+    public void setSpec() {
+        apiKey.put("api_key","2eeebe74d17da380e718f9066997a62a");
+        spec = new RequestSpecBuilder()
+                .setBaseUri("https://api.themoviedb.org")
+                .addQueryParams(apiKey)
+//                .queryParams("api_key","2eeebe74d17da380e718f9066997a62a");
+                .build();
+    }
+
+}
